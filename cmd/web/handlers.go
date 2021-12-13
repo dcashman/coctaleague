fb9fec9f0ffa13f3ff343ed17119ea7d8c99a7ea
Daniel Cashman <dan.a.cashman@gmail.com>
2021-12-13 15:21:19 -0800

    Add html templates rather than direct text output.
    Add base template, home page template, and placeholder for footer information,
    should we need it in the future.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"text/template"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -10,5 +12,26 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Hello Marketdraft users!"))
+	// UI templates which need to be composed.
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// Compose templates and write to the response. Second parameter is dynamic
+	// data, which isn't (yet) needed.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
